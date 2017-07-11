49992a805d6aa27ba4fb37b58ecf3c6c24f4d52c
Alexander Stewart <aetgy@Alexanders-MacBook-Pro.local>
2017-07-10 20:15:06 -0600

    project3

--- src/project-3.js
+++ src/project-3.js
@@ -1,6 +1,13 @@
 // Do not change any of the function names
 
 const makeCat = (name, age) => {
+  const newObj = {
+    name: `${name}`,
+    age: `${age}`,
+    meow() {
+      return 'Meow!';
+    }
+  };
   // create a new object with a name property with the value set to the name argument
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
@@ -8,61 +15,92 @@ const makeCat = (name, age) => {
 };
 
 const addProperty = (object, property) => {
+  object[property] = null;
+  return object;
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
 };
 
 const invokeMethod = (object, method) => {
+  object[method]();
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
 };
 
 const multiplyMysteryNumberByFive = (mysteryNumberObject) => {
+  return mysteryNumberObject.mysteryNumber * 5;
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
 };
 
 const deleteProperty = (object, property) => {
+  delete object[property];
+  return object;
   // remove the property from the object
   // return the object
 };
 
 const newUser = (name, email, password) => {
+  const newObj = {
+    name: `${name}`,
+    email: `${email}`,
+    password: `${password}`
+  };
   // create a new object with properties matching the arguments passed in.
   // return the new object
 };
 
 const hasEmail = (user) => {
+  if (user.email) {
+    return true;
+  }
+  return false;
   // return true if the user has a value for the property 'email'
   // otherwise return false
 };
 
 const hasProperty = (object, property) => {
+  if (object[property]) {
+    return true;
+  }
+  return false;
   // return true if the object has the value of the property argument
   // property is a string
   // otherwise return false
 };
 
 const verifyPassword = (user, password) => {
+  if (user.password === password) {
+    return true;
+  }
+  return false;
   // check to see if the provided password matches the password property on the user object
   // return true if they match
   // otherwise return false
 };
 
 const updatePassword = (user, newPassword) => {
+  user.password = newPassword;
+  return user;
   // replace the existing password on the user object with the value of newPassword
   // return the object
 };
 
 const addFriend = (user, newFriend) => {
+  user.friends.push(newFriend);
+  return user;
   // user has a property called friends that is an array
   // add newFriend to the end of the friends array
   // return the user object
 };
 
 const setUsersToPremium = (users) => {
+  for (let i = 0; i < users.length; i++) {
+    users[i].isPremium = true;
+  }
+  return users;
   // users is an array of user objects.
   // each user object has the property 'isPremium'
   // set each user's isPremium property to true
@@ -70,6 +108,11 @@ const setUsersToPremium = (users) => {
 };
 
 const sumUserPostLikes = (user) => {
+  let total = 0;
+  for (let i = 0; i < user.posts.post.length; i++) {
+    total += user.posts.post[i];
+  }
+  return total;
   // user has an array property called 'posts'
   // posts is an array of post objects
   // each post object has an integer property called 'likes'
@@ -78,6 +121,11 @@ const sumUserPostLikes = (user) => {
 };
 
 const addCalculateDiscountPriceMethod = (storeItem) => {
+  storeItem.calculateDiscountPrice = function () {
+    const discount = storeItem.price * storeItem.discountPercentage;
+    const totalDiscount = discount - storeItem.price;
+    return totalDiscount;
+  };
   // add a method to the storeItem object called 'calculateDiscountPrice'
   // this method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
   // the method then subtracts the discount from the price and returns the discounted price
