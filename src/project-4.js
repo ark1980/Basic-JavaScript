51a39e22ae0a29eaeca6277599fa02de01a438e0
Ali <ar.k@outlook.com>
2017-07-11 11:12:19 -0700

    project-04

--- src/project-4.js
+++ src/project-4.js
@@ -6,28 +6,50 @@ const multiplyArguments = () => {
   // if one argument is passed in just return it
 };
 
-const invokeCallback = (cb) => {
+const invokeCallback = cb => cb();
   // invoke cb
-};
+
 
 const sumArray = (numbers, cb) => {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  } cb(sum);
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
 };
 
 const forEach = (arr, cb) => {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    cb(newArr.push(arr[i]));
+  }
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
 };
 
 const map = (arr, cb) => {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i] * arr[i]);
+  } return newArr;
+
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
 };
 
 const getUserConstructor = () => {
+  // const User = (username, name, email, password) => {
+  //   this.username = username;
+  //   this.name = name;
+  //   this.email = email;
+  //   this.password = password;
+  //   sayHi: function() {
+  //     return `Hello, my name is ${this.name}`;
+  //   };
+  // }; return User;
   // create a constructor called User
   // it should accept an options object with username, name, email, and password properties
   // in the constructor set the username, name, email, and password properties
@@ -37,11 +59,20 @@ const getUserConstructor = () => {
 };
 
 const addPrototypeMethod = (Constructor) => {
+  Constructor.prototype.sayHi = () => 'Hello World!';
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
 };
 
 const addReverseString = () => {
+  // String.prototype.reverse = (str) => {
+  //   const newArr = str.split();
+  //   const anotherarr = [];
+  //   for (let i = newArr.length - 1; i < newArr.length; i--) {
+  //     anotherarr.push(newArr[i]);
+  //     anotherarr.join();
+  //   } return anotherarr;
+  // }
   // add a method to the string constructor's prototype that returns a reversed copy of the string
   // name this method reverse
   // hint:
@@ -49,6 +80,10 @@ const addReverseString = () => {
 };
 
 const nFactorial = (n) => {
+  let total = 1;
+  for (let i = n; i >= 1; i--) {
+    total *= i;
+  } return total;
   // return the factorial for n
   // solve this recursively
   // example:
